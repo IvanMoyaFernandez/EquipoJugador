9338dd0cab3931e8f74f1f4ff4630e0380ce9fc0
Ivan Moya <ivanmoya95@gmail.com>
2016-11-08 17:39:40 +0100

    EquipoRepository modificado, todo sigue funcionando!

--- src/main/java/com/moya/Repository/EquipoRepository.java
+++ src/main/java/com/moya/Repository/EquipoRepository.java
@@ -8,9 +8,11 @@ import java.util.List;
 
 @Repository
 public interface EquipoRepository extends JpaRepository<Equipo, Long>  {
+
+// Para registrar el equipo en cada jugador. --> Utilizado en JugadorService
+    public List<Equipo> findByNombre(String nombreEquipo);
+
 // PT 1 EJERCICIO 2 ENTIDAD EQUIPO
     // A
     public List<Equipo> findByLocalidad(String localidadEquipo);
-    // B
-    public List<Equipo> findByNombre(String nombreEquipo);
 }
